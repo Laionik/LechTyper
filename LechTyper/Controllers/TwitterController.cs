da6af5363440d0e14c98f98e11c9bdab4f136f5c
Laionik <lukasz.sosinski4@gmail.com>
2015-05-19 14:59:17 +0200

    Wczytywanie tweetów

--- LechTyper/Controllers/TwitterController.cs
+++ LechTyper/Controllers/TwitterController.cs
@@ -8,6 +8,7 @@ using Newtonsoft.Json.Linq;
 using LechTyper.OAuth;
 using System.Net.Http;
 using System.Threading.Tasks;
+using LechTyper.Models;
 
 namespace LechTyper.Controllers
 {
@@ -18,11 +19,10 @@ namespace LechTyper.Controllers
 
         public async Task<ActionResult> Twitter()
         {
-            JToken tweets = await RunClient();
-            var results = tweets["statuses"].Children().ToList();
+            var tweets = await RunClient();
             if (tweets != null)
             {
-                return View(results);
+                return View(tweets);
             }
             else
             {
@@ -31,28 +31,40 @@ namespace LechTyper.Controllers
         }
 
 
-        async Task<JToken> RunClient()
+        async Task<List<ReadTwitter>> RunClient()
         {
-            string _address = "https://api.twitter.com/1.1/search/tweets.json?q=RAZEMpoMistrzostwo&count=100";
+            string _address = "https://api.twitter.com/1.1/search/tweets.json?q=czterydychylecha&count=100";
             HttpClient client = new HttpClient(new OAuthMessageHandler(new HttpClientHandler()));
             HttpResponseMessage response = await client.GetAsync(_address);
-            int x;
+            List<ReadTwitter> tweets = new List<ReadTwitter>();
+            ReadTwitter previous = null;
             if (response.IsSuccessStatusCode)
             {
                 JToken statuses = await response.Content.ReadAsAsync<JToken>();
+                foreach (var x in statuses["statuses"])
+                {
+                    tweets.Add(new ReadTwitter(x["created_at"].ToString(), x["id_str"].ToString(), x["text"].ToString(), x["user"]["id_str"].ToString(), x["user"]["name"].ToString(), x["user"]["screen_name"].ToString()));
+                }
 
                 do
                 {
-                    var child = statuses["statuses"].Last();
-                    _address = "https://api.twitter.com/1.1/search/tweets.json?q=RAZEMpoMistrzostwo&count=100&max_id=" + child["id_str"].ToString();
+                    var last = tweets.Last();
+                    if (last == previous)
+                        break;
+                    else
+                        previous = last;
+                    _address = "https://api.twitter.com/1.1/search/tweets.json?q=czterydychylecha&count=100&max_id=" + last.post_id;
                     response = await client.GetAsync(_address);
                     if (response.IsSuccessStatusCode)
                     {
-                        statuses.AddAfterSelf(await response.Content.ReadAsAsync<JToken>()); //COŚ JEST ZJE***
+                        statuses = await response.Content.ReadAsAsync<JToken>();
+                        foreach (var x in statuses["statuses"])
+                        {
+                            tweets.Add(new ReadTwitter(x["created_at"].ToString(), x["id_str"].ToString(), x["text"].ToString(), x["user"]["id_str"].ToString(), x["user"]["name"].ToString(), x["user"]["screen_name"].ToString()));
+                        }
                     }
-                    x = statuses["statuses"].Count();
-                } while (statuses.Count() % 100 == 0 && statuses.Count() != 0);
-                return statuses;
+                } while (true);
+                return tweets;
             }
             return null;
         }
